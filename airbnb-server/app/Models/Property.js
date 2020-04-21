854649c1fc25fb1cd532f45dd766a936b74fe50f
Shibata18 <douglas.shibata11@gmail.com>
2020-04-21 09:50:23 -0300

    Upload e geolocalização

--- airbnb-server/app/Models/Property.js
+++ airbnb-server/app/Models/Property.js
@@ -1,13 +1,25 @@
 'use strict'
-
+const Database = use('Database');
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Property extends Model {
-    user(){
+    static scopeNearBy(query, latitude, longitude, distance) {
+        const haversine = `(6371 * acos(cos(radians(${latitude}))
+        * cos(radians(latitude))
+        * cos(radians(longitude)
+        - radians(${longitude}))
+        + sin(radians(${latitude}))
+        * sin(radians(latitude))))`
+
+        return query
+            .select('*', Database.raw(`${haversine} as distance`))
+            .whereRaw(`${haversine} < ${distance}`)
+    }
+    user() {
         return this.belongsTo('App/Models/User')
     }
-    images(){
+    images() {
         return this.hasMany('App/Models/Image')
     }
 }
--- airbnb-server/start/routes.js
+++ airbnb-server/start/routes.js
@@ -24,3 +24,5 @@ Route.post('/sessions','SessionController.create');
 Route.resource('properties', 'PropertyController')
   .apiOnly()
   .middleware('auth')
+Route.post('properties/:id/images','ImageController.store').middleware("auth")
+Route.get('images/:path', 'ImageController.show')
